485eb65d6a217846faa4eb1c728d928619822232
Skhendle <skhendle@gmail.com>
2026-01-04 09:45:30 +0000

    comments for classes and functions

--- src/generic_models/app_error.py
+++ src/generic_models/app_error.py
@@ -1,10 +1,10 @@
 from pydantic import BaseModel
 
-
 class DetailModel(BaseModel):
+    """Model representing error details."""
     message: str
     error: str
 
-
 class AppErrorResponseModel(BaseModel):
+    """Model representing an application error response."""
     detail: DetailModel
--- src/generic_models/generic_response.py
+++ src/generic_models/generic_response.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 
 T = TypeVar("T")
 
-
 class GenericResponseModel(BaseModel, Generic[T]):
+    """Generic response model wrapping a message and optional data."""
     message: str
     data: Optional[T]
